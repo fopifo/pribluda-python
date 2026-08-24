3fffb39806e516e8b984a35a208373af03754c39
BIGGART <fopifo81@yandex.ru>
2026-08-24 07:17:37 +0300

    батник v2: запускает iss_quotes_sync и main.py сразу

--- Приблуда_Quik.bat
+++ Приблуда_Quik.bat
@@ -1,2 +1,5 @@
 @echo off
-start /min "" python.exe main.py --source quik
+rem v2 (2026-08-24): start iss_quotes_sync + main.py together
+cd /d "%~dp0"
+start /min "ISS_Quotes" python.exe tools/iss_quotes_sync.py
+start /min "Pribluda_Quik" python.exe main.py --source quik
