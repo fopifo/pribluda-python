ff78eea866028af8a4e4a2af03eb31d60df55bf0
BIGGART <fopifo81@yandex.ru>
2026-09-03 03:06:18 +0300

    чистка

--- Приблуда_Quik.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem v2 (2026-08-24): start iss_quotes_sync + main.py together
-cd /d "%~dp0"
-start /min "ISS_Quotes" python.exe tools/iss_quotes_sync.py
-start /min "Pribluda_Quik" python.exe main.py --source quik
